fb0d6f670a1c53b8d4cc05914754836207441202
Vinicius Moraes <vmrc@ding-an-sich.com>
2020-08-25 15:25:29 -0300

    Beautify

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,11 +1,11 @@
 class TasksController < ApplicationController
+  before_action :set_task, only: %i[show edit update destroy]
+
   def index
     @tasks = Task.all
   end
 
-  def show
-    @task = Task.find(params[:id])
-  end
+  def show; end
 
   def new
     @task = Task.new
@@ -16,9 +16,25 @@ class TasksController < ApplicationController
     redirect_to task_path(@task.id)
   end
 
+  def edit; end
+
+  def update
+    @task.update(strong_params)
+    redirect_to task_path(@task.id)
+  end
+
+  def destroy
+    @task.destroy
+    redirect_to tasks_path
+  end
+
   private
 
+  def set_task
+    @task = Task.find(params[:id])
+  end
+
   def strong_params
-    params.require(:task).permit(:title, :details)
+    params.require(:task).permit(:title, :details, :completed)
   end
 end
